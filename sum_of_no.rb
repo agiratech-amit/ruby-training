accae592b87d7afbd55374f08b5b820b3a815c5e
Agira <amit@agiratech.com>
2016-10-18 12:28:29 +0530

    updatedsum_of_no.rb

--- sum_of_no.rb
+++ sum_of_no.rb
@@ -1,20 +1,20 @@
 #Sum of digits of a number program using recursion
 
-class Add
-	
-	
-def sum_of_no num
+class Add	
+  def sum_of_no num
     temp=0
     while num>0
-    temp+=(num%10)
-    num=num/10
+      temp+=(num%10)
+      num=num/10
     end
-   return temp
+    return temp
+  end
 end
-end
-p "enter any number"
+
+print "enter any number"
 num=gets.to_i
-obj=Add.new  #creating object
+#creating object
+obj=Add.new  
 print "the sum of no is=\n#{obj.sum_of_no num}\n" #passing argument and printing return value
    
 
